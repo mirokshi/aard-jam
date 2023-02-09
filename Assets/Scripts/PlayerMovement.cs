f08a6cc20776af47f64a6736b2bbf70e919b03d7
Mirokshi <rojasdiazmirokshi@gmail.com>
2023-02-09 18:46:58 +0100

    --

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        
-        transform.Translate(new Vector3(horizontal,0.0f,vertical)*Time.deltaTime*speed);
-        
-    }
+   
 }
