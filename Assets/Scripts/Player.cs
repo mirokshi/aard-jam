5d6ae01b9dc6581fed39eb71d6fae10fb7bb857f
Mirokshi <rojasdiazmirokshi@gmail.com>
2023-02-10 16:06:46 +0100

    Mouse Controller (ahora si)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,15 +15,4 @@ public class Player : MonoBehaviour
 
         DontDestroyOnLoad(gameObject);
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
