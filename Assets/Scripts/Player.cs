be19dd8ff42f912f992a09b6b01126e7c213ee0e
Mirokshi <rojasdiazmirokshi@gmail.com>
2023-02-10 15:48:44 +0100

    Mouse controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player playerInstance;
+
+    private void Awake()
+    {
+        if (playerInstance == null)
+            playerInstance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
