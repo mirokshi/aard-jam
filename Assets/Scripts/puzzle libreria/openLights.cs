383e5406c6af5a08c342aba6f327b081f3a0756e
Campalans03 <jancampalans@gmail.com>
2023-02-12 09:55:42 +0100

    Indicador llums biblioteca

---
+++ Assets/Scripts/puzzle libreria/openLights.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openLights : MonoBehaviour
+{
+    [SerializeField] private int llumsObertes;
+    [SerializeField] private int numeroLlum;
+    private Renderer colorLuz;
+
+    private void OnEnable()
+    {
+        detectarClick.buttonCliked += llum;
+        paswordControl.onOpenDoor += llumVerd;
+        paswordControl.onCloseDoor += llumVermella;
+        colorLuz = GetComponent<Renderer>();
+        llumsObertes = 0;
+    }
+
+    private void OnDisable()
+    {
+        detectarClick.buttonCliked -= llum;
+        paswordControl.onOpenDoor -= llumVerd;
+        paswordControl.onCloseDoor -= llumVermella;
+    }
+
+    private void llum(detectarClick obj)
+    {
+        llumsObertes++;
+        if (llumsObertes == numeroLlum)
+        {
+            colorLuz.material.SetColor("_Color", Color.yellow);
+        }
+        
+    }
+
+    private void llumVerd()
+    {
+        llumsObertes = 0;
+        colorLuz.material.SetColor("_Color", Color.green);
+    }
+
+    private void llumVermella()
+    {
+        llumsObertes = 0;
+        colorLuz.material.SetColor("_Color", Color.red);
+    }
+}
